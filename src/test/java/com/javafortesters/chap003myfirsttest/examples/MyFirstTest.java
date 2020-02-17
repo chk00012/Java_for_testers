175ffcc8bf264eed2794b8ef3eaffc8ab56209a5
chris.kerr <chris.kerr@modulrfinance.com>
2020-02-17 16:18:04 +0000

    hex tests added

--- src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
@@ -27,6 +27,28 @@ public class MyFirstTest {
     public void integerFour() {
         Integer four = new Integer(4);
         assertEquals("4 is returned",4, four.intValue());
-
     }
+
+    @Test
+    public void hexint1() {
+        Integer hex = 11;
+        assertEquals("that 11 becomes b","b", hex.toHexString(hex));
+    }
+    @Test
+    public void hexint2() {
+        Integer hex = 10;
+        assertEquals("that 10 becomes a", "a", hex.toHexString(hex));
+    }
+    @Test
+    public void hexint3() {
+        Integer hex = 3;
+        assertEquals("that 3 becomes 3", "3", hex.toHexString(hex));
+    }
+    @Test
+    public void hexint4() {
+        Integer hex = 21;
+        assertEquals("that 21 becomes 15", "15", hex.toHexString(hex));
+    }
+
+
 }
