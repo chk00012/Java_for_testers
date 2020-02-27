2a8bd2d423f504c1bd39c769b74a57d659067923
chk00012 <48138202+chk00012@users.noreply.github.com>
2020-02-27 17:54:51 +0000

    practice

--- src/test/java/com/javafortesters/chap005testwithourownclasses/domainobject/example/TestAppEnvironmentTest.java
+++ src/test/java/com/javafortesters/chap005testwithourownclasses/domainobject/example/TestAppEnvironmentTest.java
@@ -1,14 +1,21 @@
 package com.javafortesters.chap005testwithourownclasses.domainobject.example;
 
+import com.javafortesters.chap005testwithourownclasses.domainobject.TestAppEnv;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestAppEnvironmentTest {
     @Test
     public void canGetUrlStatically() {
-        assertEquals("Return Hard Coded URL", "https://192.123.0.3:67", TestAppEnv.getUrl());
-
+    assertEquals("Returns Hard Coded URL", "https://192.123.0.3:67", TestAppEnv.getUrl());
     }
+
+    @Test
+    public void canGetDomainAndPortStatically() {
+        assertEquals("Just the Domain", "192.123.0.3", TestAppEnv.DOMAIN);
+        assertEquals("Just the port", "67",TestAppEnv.PORT);
+    }
+
 
 
 }
