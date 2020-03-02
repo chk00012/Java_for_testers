e94cd603851118c9ff77e3758a18d97c412f12e9
chris.kerr <chris.kerr@modulrfinance.com>
2020-03-02 13:42:25 +0000

    User test class created

---
+++ src/test/java/com/javafortesters/chap006domainentities/examples/UserTest.java
@@ -0,0 +1,11 @@
+package com.javafortesters.chap006domainentities.examples;
+
+import com.javafortesters.domainentities.User;
+import org.junit.Test;
+
+public class UserTest {
+    @Test
+    public void canConstructANewUser() {
+        User user = new User();
+    }
+}
